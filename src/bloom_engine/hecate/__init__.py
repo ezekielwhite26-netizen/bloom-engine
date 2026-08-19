29b00724b4edb57c9304f5da18c81c1452f6bd8c
ezekielwhite26-netizen <ezekielwhite26@gmail.com>
2026-08-19 07:10:55 -0400

    Export persisted HECATE adapter and integrity catalog

--- src/bloom_engine/hecate/__init__.py
+++ src/bloom_engine/hecate/__init__.py
@@ -1,13 +1,23 @@
+from bloom_engine.hecate.airtable_store import AirtableHecateStore, AirtableSnapshot
+from bloom_engine.hecate.integrity_catalog import HECATE_INTEGRITY_TESTS
 from bloom_engine.hecate.models import FocusBond, MagicForm, MagicalCondition, MagicProfile
 from bloom_engine.hecate.resolver import HecateResolver
 from bloom_engine.hecate.store import HecateStore, InMemoryHecateStore
+from bloom_engine.hecate.working_rules import FOCUS_DOCTRINE, HEAT_DOCTRINE, PassageFixture, assess_passage_focus_fixture
 
 __all__ = [
+    "AirtableHecateStore",
+    "AirtableSnapshot",
     "FocusBond",
+    "FOCUS_DOCTRINE",
+    "HEAT_DOCTRINE",
+    "HECATE_INTEGRITY_TESTS",
     "HecateResolver",
     "HecateStore",
     "InMemoryHecateStore",
     "MagicForm",
-    "MagicProfile",
     "MagicalCondition",
+    "MagicProfile",
+    "PassageFixture",
+    "assess_passage_focus_fixture",
 ]
