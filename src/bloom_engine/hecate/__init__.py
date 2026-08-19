b4e2325310ae95921640224f27162e58a7fd0b1c
ezekielwhite26-netizen <ezekielwhite26@gmail.com>
2026-08-19 06:58:03 -0400

    Expose HECATE resolver interfaces

---
+++ src/bloom_engine/hecate/__init__.py
@@ -0,0 +1,13 @@
+from bloom_engine.hecate.models import FocusBond, MagicForm, MagicalCondition, MagicProfile
+from bloom_engine.hecate.resolver import HecateResolver
+from bloom_engine.hecate.store import HecateStore, InMemoryHecateStore
+
+__all__ = [
+    "FocusBond",
+    "HecateResolver",
+    "HecateStore",
+    "InMemoryHecateStore",
+    "MagicForm",
+    "MagicProfile",
+    "MagicalCondition",
+]
